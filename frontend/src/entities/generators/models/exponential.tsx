e1e32a253307bcc7cf462b81cacf18a933172fd5
Rcon55 <rcon55@gmail.com>
2023-02-13 18:32:10 +0700

    Change ui

--- frontend/src/entities/generators/models/exponential.tsx
+++ frontend/src/entities/generators/models/exponential.tsx
@@ -1,39 +1,94 @@
-import { Box, Grid, TextField, Typography } from '@mui/material'
+import { Box, Button, Grid, TextField, Typography } from '@mui/material'
 import React from 'react'
 
 interface ExponentialModelProps {
-	numSamples: React.MutableRefObject<number>,
-	midPor: React.MutableRefObject<number>,
-	devPor: React.MutableRefObject<number>,
+	modelSettings: Function
 }
 
-export const ExponentialModel = () => {
+export const ExponentialModel = ({modelSettings}: ExponentialModelProps) => {
 	return (
-		<Grid container spacing={2}>
-			<Grid item md={6}>
-				<Typography></Typography>
-			</Grid>
+		<Grid container spacing={2} >
 			<Grid item md={6}>
 				<TextField 	id="numSamples" 
 							type="number" 
-							label="Количество образцов" 
+							label="Кол-во обр." 
+							onChange={(event) => modelSettings('numSamples', event.target.value)}
 							variant="standard" />
+			</Grid>
+
+			<Grid sx={{display: 'flex', alignItems: 'end', justifyContent: 'center'}}>
+				<Button
+					variant="outlined"
+					fullWidth
+					
+					sx={{ width: '100%', bottom: 0}}
+					onClick={() => console.log(modelSettings)}
+					>Создать
+				</Button>
 			</Grid>
 			
 			{/* Настройки для пористости */}
+			<Grid item md={12}>
+				<Typography>
+					Настройки модели пористости
+				</Typography>
+			</Grid>
 			<Grid item md={6}>
 				<TextField 	id="midPor" 
 							type="number" 
-							label="Средняя пористость" 
+							label="Ср. пористость" 
+							onChange={(event) => modelSettings('midPor', event.target.value)}
 							variant="standard" />
 			</Grid>
 			<Grid item md={6}>
 				<TextField 	id="devPor" 
 							type="number" 
-							label="Отклонение значений" 
+							label="Разброс знач." 
+							onChange={(event) => modelSettings('devPor', event.target.value)}
 							variant="standard" />
 			</Grid>
+
 			{/* Настройки для проницаемости */}
+			<Grid item md={12}>
+				<Typography>
+					Настройки модели проницаемости
+				</Typography>
+			</Grid>
+			<Grid item md={6}>
+				<TextField 	id="minPor" 
+							type="number" 
+							label="Пористость T1" 
+							onChange={(event) => modelSettings('minPor', event.target.value)}
+							variant="standard" />
+			</Grid>
+			<Grid item md={6}>
+				<TextField 	id="minPerm" 
+							type="number" 
+							label="Проницаемость T1" 
+							onChange={(event) => modelSettings('minPerm', event.target.value)}
+							variant="standard" />
+			</Grid>
+			<Grid item md={6}>
+				<TextField 	id="maxPor" 
+							type="number" 
+							label="Пористость T2"
+							onChange={(event) => modelSettings('maxPor', event.target.value)}
+							variant="standard" />
+			</Grid>
+			<Grid item md={6}>
+				<TextField 	id="maxPerm" 
+							type="number" 
+							label="Проницаемость T2" 
+							onChange={(event) => modelSettings('maxPerm', event.target.value)}
+							variant="standard" />
+			</Grid>
+			<Grid item md={6}>
+				<TextField 	id="devPerm" 
+							type="number" 
+							label="Разброс знач." 
+							onChange={(event) => modelSettings('devPerm', event.target.value)}
+							variant="standard" />
+			</Grid>
 		</Grid>
 	)
 }
