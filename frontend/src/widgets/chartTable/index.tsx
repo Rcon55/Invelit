e2193237b627fd00cf51962c1785a29bd9df3cde
Rcon55 <rcon55@gmail.com>
2023-02-27 09:15:24 +0700

    Add selections in table

--- frontend/src/widgets/chartTable/index.tsx
+++ frontend/src/widgets/chartTable/index.tsx
@@ -5,21 +5,11 @@ import { TableMenu } from './menu'
 import { Table } from './table'
 
 export const ChartTable = () => {
-	const activeTable = useTypedSelector(state => state.states.activeDataTable)
-	const table = getTable(activeTable)
-	const columns = table && table[0] && getHeader(activeTable)
-	const pk = table && table[0] && getIdColumn(activeTable)
 
 	return (
 		<div>
 			<TableMenu />
-			{(!table || !table[0] || !columns || !pk) ? false :
-				<Table 
-				table={table}
-				columns={columns}
-				pk={pk}
-				/>
-			}
+			<Table />
 		</div>
 	)
 }
